225ba5ddb2fc413d1ed98158bfe004583ae4028d
Luka Mirosevic <luka@goonbee.com>
2014-06-12 15:40:58 +0200

    Added zeromq package installer

--- zeromq/recipes/install.rb
+++ zeromq/recipes/install.rb
@@ -1,6 +1,7 @@
-yum_repository 'fengshuo' do
-  description 'fengshuo'
-  baseurl 'http://download.opensuse.org/repositories/home:/fengshuo:/zeromq/CentOS_CentOS-6/home:fengshuo:zeromq.repo'
+repository 'home_fengshuo_zeromq' do
+  description 'The latest stable of zeromq builds (CentOS_CentOS-6)'
+  baseurl 'http://download.opensuse.org/repositories/home:/fengshuo:/zeromq/CentOS_CentOS-6/'
+  gpgkey 'http://download.opensuse.org/repositories/home:/fengshuo:/zeromq/CentOS_CentOS-6/repodata/repomd.xml.key'
   action :create
 end
 
