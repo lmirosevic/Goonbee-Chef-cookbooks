264ef2934192728904f52b7b0989283cab952244
Luka Mirosevic <luka@goonbee.com>
2014-06-14 11:02:07 +0200

    Fixed node restarting

--- app_config/recipes/generate.rb
+++ app_config/recipes/generate.rb
@@ -29,7 +29,13 @@ node[:deploy].each do |application, deploy|
           notifies :run, "execute[restart Rails app #{application}]"
         elsif deploy[:application_type] == 'nodejs'
           # restart node app
-          notifies :restart, 'service[monit]', :immediately
+          ruby_block "restart node.js application #{application}" do
+            block do
+              Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
+              Chef::Log.info(`#{node[:deploy][application][:nodejs][:restart_command]}`)
+              $? == 0
+            end
+          end
         end
 
         only_if do
